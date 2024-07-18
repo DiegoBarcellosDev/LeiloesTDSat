68cbad1641f080cad4765ed679b6c1b64da2e0b7
Diego Barcellos <diegobarcellos.dev@gmail.com>
2024-07-18 14:54:22 -0300

    Definição da tela principal e botão de salvar funcionando.

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -7,36 +7,43 @@
  *
  * @author Adm
  */
-
 import java.sql.PreparedStatement;
 import java.sql.Connection;
-import javax.swing.JOptionPane;
 import java.sql.ResultSet;
 import java.util.ArrayList;
-
+import java.sql.SQLException;
 
 public class ProdutosDAO {
-    
+
     Connection conn;
-    PreparedStatement prep;
+    PreparedStatement st;
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
-    
-    public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+
+    public int cadastrarProduto(ProdutosDTO produto) {
+
+        conn = conectaDAO.connectDB();
+        int status;
+        String sql = "INSERT INTO produtos (nome, valor, status) VALUES(?,?,?)";
+        try {
+
+            st = conn.prepareStatement(sql);
+            st.setString(1, produto.getNome());
+            st.setInt(2, produto.getValor());
+            st.setString(3, produto.getStatus());
+            status = st.executeUpdate();
+            return status;
+
+        } catch (SQLException ex) {
+            System.out.println("Erro ao conectar: ");
+            return ex.getErrorCode();
+        }
+
     }
-    
-    public ArrayList<ProdutosDTO> listarProdutos(){
-        
+
+    public ArrayList<ProdutosDTO> listarProdutos() {
+
         return listagem;
     }
-    
-    
-    
-        
-}
 
+}
